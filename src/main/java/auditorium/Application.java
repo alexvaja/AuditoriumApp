22d3954110c0cdbe459cbe9606ea418a0222d85e
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-03-03 01:10:06 +0200

    Added Application class - > main class

---
+++ src/main/java/auditorium/Application.java
@@ -0,0 +1,17 @@
+package auditorium;
+
+//import org.apache.log4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+// static Logger LOGGER = Logger.getLogger(Application.class);
+
+	public static void main(final String[] args) {
+		// LOGGER.info("STARTING DINER APP");
+		SpringApplication.run(Application.class, args);
+	}
+}
