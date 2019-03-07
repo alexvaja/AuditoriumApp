f4b166d7f4d0b64abb7cb6d2de08227492625137
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-03-07 17:16:58 +0200

    Finished main class and added Logger with message

--- src/main/java/auditorium/Application.java
+++ src/main/java/auditorium/Application.java
@@ -1,17 +1,18 @@
 package auditorium;
 
-//import org.apache.log4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 public class Application {
 
-// static Logger LOGGER = Logger.getLogger(Application.class);
+	private final static Logger LOGGER = LoggerFactory.getLogger(Application.class.getClass());
 
 	public static void main(final String[] args) {
-		// LOGGER.info("STARTING DINER APP");
 		SpringApplication.run(Application.class, args);
+		LOGGER.info("--- START AUDITORIUM APP ---");
 	}
 }
