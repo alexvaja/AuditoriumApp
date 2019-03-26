23fe78a41263da431ff15cffd45e827949bbee2f
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-03-26 13:48:33 +0200

    Add Type class with all attributes and relation between entities.

---
+++ src/main/java/auditorium/model/entity/Type.java
@@ -0,0 +1,31 @@
+package auditorium.model.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "type")
+public class Type {
+
+	@Id
+	@Column(name = "type_id", length = 5, nullable = false, unique = true)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "type", length = 60, nullable = false, unique = true)
+	private String type;
+	
+	@Column(name = "description", length = 120)
+	private String description;
+	//
+	@ManyToMany(mappedBy = "type", fetch = FetchType.EAGER)
+	private List<Class> classes;
+}
