434f9a8d62c652299fcdcce8f3572f5aade2fd26
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-04-02 22:49:12 +0300

    Finished seatbooking Class.

--- src/main/java/auditorium/model/entity/SeatBooking.java
+++ src/main/java/auditorium/model/entity/SeatBooking.java
@@ -1,10 +1,30 @@
 package auditorium.model.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "seat_booking")
 public class SeatBooking {
 
+	@Id
+	@Column(name = "id_seat_booking")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private AppUser appUser;
+	
+	@JoinColumn(name = "room_booking_id", nullable = false)
+	private RoomBooking roomBooking;
+	
+	@Column(name = "seat_number", length = 60, nullable = false)
+	private String seatNumber;
 }
