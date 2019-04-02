6fc1898a0758faf07c98610a329e2c301bcc095b
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-04-02 22:49:46 +0300

    And finish this class with all attributes.

--- src/main/java/auditorium/model/entity/Category.java
+++ src/main/java/auditorium/model/entity/Category.java
@@ -1,5 +1,31 @@
 package auditorium.model.entity;
 
-public class Category {
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category 
+{
+	@Id
+	@Column(name = "id_category")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "price")
+	private Double price;
+	//
+	@OneToMany(mappedBy = "category")
+	private List<Dish> dishes;
 
 }
