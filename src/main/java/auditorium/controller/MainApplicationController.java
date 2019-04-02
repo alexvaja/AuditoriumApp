1d122f134226d0172bf56a741261ebef6c553f62
silviu_alex <sabau.silviu.bv@gmail.com>
2019-04-02 16:04:48 +0300

    Am adaugat selectia de facultate pe pagina de register

--- src/main/java/auditorium/controller/MainApplicationController.java
+++ src/main/java/auditorium/controller/MainApplicationController.java
@@ -3,7 +3,6 @@ package auditorium.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,7 +14,7 @@ public class MainApplicationController {
 		return "index";
 	}
 
-	@PostMapping("/index")
+	@PostMapping("/")
 	public String startAppByIndex(@RequestParam("submit") String reqParam) {
 		switch (reqParam) 
 		{
