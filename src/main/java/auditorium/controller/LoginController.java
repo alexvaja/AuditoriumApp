1c07f5e49ac2b086829424df106df3b02d45ceb5
silviu_alex <sabau.silviu.bv@gmail.com>
2019-04-04 19:13:48 +0300

    Am scos LoginController din Ignored.

---
+++ src/main/java/auditorium/controller/LoginController.java
@@ -0,0 +1,16 @@
+package auditorium.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController 
+{
+	@GetMapping("/LoginView")
+	public String startApp() 
+	{
+		System.out.println("Am intrat in login!");
+		return "views/LoginView";
+	}
+
+}
--- src/main/java/auditorium/controller/MainApplicationController.java
+++ src/main/java/auditorium/controller/MainApplicationController.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import auditorium.Application;
-
 @Controller
 public class MainApplicationController {
 
