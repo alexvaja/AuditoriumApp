ea36c572eddf2f5bea59c100c8be93498af42509
silviu_alex <sabau.silviu.bv@gmail.com>
2019-05-13 12:17:42 +0300

    Finished the login part with security. You can log out from the app

--- src/main/java/smartCardUniversity/LOGIN/controllers/LoginController.java
+++ src/main/java/smartCardUniversity/LOGIN/controllers/LoginController.java
@@ -1,20 +1,36 @@
 package smartCardUniversity.LOGIN.controllers;
+import java.security.Principal;
+
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.annotation.SessionScope;
 
 import smartCardUniversity.SHARED.exceptions.NewSessionException;
+import smartCardUniversity.SHARED.utils.WebUtils;
 
 @Controller
 public class LoginController 
 {
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminPage(Model model, Principal principal) {
+		LOGGER.info("am intrat mai ceva ca Hitler in Polonia cu panzerul");
+		User loginedUser = (User) ((Authentication) principal).getPrincipal();
+		String userInfo = WebUtils.toString(loginedUser);
+		model.addAttribute("userInfo", userInfo);
+		return "adminPage";
+	}
+	
 	@SessionScope
 	@GetMapping({"/", "/LoginView"})
 	public String openLoginPage(Model model, HttpSession session) throws NewSessionException 
@@ -22,5 +38,10 @@ public class LoginController
 		LOGGER.info("Am intrat pe pagina de login prin Getmapping");
 		return "views/shared/LoginView";
 	}
-
+	
+	@RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
+	public String logoutSuccessfulPage(Model model) {
+		return "views/shared/loginView";
+	}
+	
 }
--- src/main/java/smartCardUniversity/SHARED/security/config/WebSecurityConfig.java
+++ src/main/java/smartCardUniversity/SHARED/security/config/WebSecurityConfig.java
@@ -38,13 +38,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		// The pages does not require login
 		http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
-		
+		http.authorizeRequests().antMatchers("/index").authenticated();
 		// /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
 		// If no login, it will redirect to /login page.
 		//http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
 
 		// For ADMIN only.
-		//http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
+		http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
 
 		// When the user has logged in as XX.
 		// But access a page that requires role YY,
