c1b4aa6e941e811c7309f98654c55f3ef0d9c35f
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-05-13 11:16:36 +0300

    Added RegisterDTO.

--- src/main/java/smartCardUniversity/SHARED/controller/RegisterController.java
+++ src/main/java/smartCardUniversity/SHARED/controller/RegisterController.java
@@ -3,6 +3,7 @@ package smartCardUniversity.SHARED.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class RegisterController 
