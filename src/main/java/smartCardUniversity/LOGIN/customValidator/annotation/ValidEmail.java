afd75e555e2c52b061d696ea33176d833abce093
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-03 00:56:20 +0300

    Finished annotation for validation email. Annotation have 3 method: message, groups and payload.

--- src/main/java/smartCardUniversity/LOGIN/customValidator/annotation/ValidEmail.java
+++ src/main/java/smartCardUniversity/LOGIN/customValidator/annotation/ValidEmail.java
@@ -1,5 +1,23 @@
 package smartCardUniversity.LOGIN.customValidator.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import smartCardUniversity.LOGIN.customValidator.EmailValidator;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailValidator.class)
+@Documented
 public @interface ValidEmail {
-//TODO
+
+	String message() default "Invalid email";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
 }
