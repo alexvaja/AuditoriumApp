89378cec4564e2e70b7ffb86f3c749c8720b3521
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-03 01:08:45 +0300

    Implemented validator for email.

--- src/main/java/smartCardUniversity/LOGIN/customValidator/EmailValidator.java
+++ src/main/java/smartCardUniversity/LOGIN/customValidator/EmailValidator.java
@@ -1,5 +1,30 @@
 package smartCardUniversity.LOGIN.customValidator;
 
-public class EmailValidator {
-//TODO
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import smartCardUniversity.LOGIN.customValidator.annotation.ValidEmail;
+
+public class EmailValidator implements ConstraintValidator<ValidEmail, String> {//TODO
+
+	private Pattern pattern;
+	
+	private Matcher matcher;
+	//TODO de refacut acesta
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@" + 
+	"[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$"; 
+	
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		return validateEmail(value);
+	}
+	
+	private boolean validateEmail(String email) {
+		pattern = Pattern.compile(EMAIL_PATTERN);
+		matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
 }
