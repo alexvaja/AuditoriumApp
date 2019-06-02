efd6d58b2fff23991241b396250a9b75ce9f8653
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-02 16:24:45 +0300

    Implement binding result/errors for register action.

--- src/main/java/smartCardUniversity/LOGIN/controllers/RegisterController.java
+++ src/main/java/smartCardUniversity/LOGIN/controllers/RegisterController.java
@@ -1,19 +1,45 @@
 package smartCardUniversity.LOGIN.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import smartCardUniversity.LOGIN.models.DTO.RegisterDTO;
 
 @Controller
-public class RegisterController 
-{
-	@GetMapping("/RegisterView")
-	public String getMapping(Model model)
-	{
-		return "views/shared/RegisterView";
-	}
+public class RegisterController implements WebMvcConfigurer {
 	
-	public void x() {
+	private static String VIEW_PATH = "views/shared/RegisterView";
+	
+	@GetMapping("/RegisterView")
+	public ModelAndView getMapping(Model model) {
+	
+		ModelAndView modelView = new ModelAndView(VIEW_PATH);
+		modelView.addObject("newUser", new RegisterDTO());
+
+		return modelView;
+	}
+
+	@PostMapping("/RegisterView")
+	private ModelAndView postMethod(Model model, @Valid @ModelAttribute("newUser") RegisterDTO registerDTO, BindingResult result) {
+
+		ModelAndView modelAndView = new ModelAndView(VIEW_PATH);
+		modelAndView.addObject("newUser", registerDTO);
 		
+	    if (result.hasErrors()) {
+	    	System.out.println("-- Am intrat in if --");
+	        return modelAndView;
+	    }
+
+		System.out.println("MODEL: " + registerDTO);
+
+		return modelAndView;
 	}
 }
