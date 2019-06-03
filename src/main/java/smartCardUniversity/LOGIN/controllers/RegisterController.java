438d199fcaf9923f40e60859c4d93698895c698d
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-03 21:17:51 +0300

    Added in post method  in list of parametters Errors And WebRequest objects.

--- src/main/java/smartCardUniversity/LOGIN/controllers/RegisterController.java
+++ src/main/java/smartCardUniversity/LOGIN/controllers/RegisterController.java
@@ -1,13 +1,18 @@
 package smartCardUniversity.LOGIN.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,28 +20,40 @@ import smartCardUniversity.LOGIN.models.DTO.RegisterDTO;
 
 @Controller
 public class RegisterController implements WebMvcConfigurer {
-	
+
 	private static String VIEW_PATH = "views/shared/RegisterView";
-	
+
 	@GetMapping("/RegisterView")
 	public ModelAndView getMapping(Model model) {
-	
+
 		ModelAndView modelView = new ModelAndView(VIEW_PATH);
 		modelView.addObject("newUser", new RegisterDTO());
 
 		return modelView;
-	} 
+	}
 
 	@PostMapping("/RegisterView")
-	private ModelAndView postMethod(Model model, @Valid @ModelAttribute("newUser") RegisterDTO registerDTO, BindingResult result) {
+	private ModelAndView postMethod(Model model, @ModelAttribute("newUser") @Valid RegisterDTO registerDTO,
+			BindingResult result, WebRequest request, Errors errors) {
 
 		ModelAndView modelAndView = new ModelAndView(VIEW_PATH);
 		modelAndView.addObject("newUser", registerDTO);
-		
-	    if (result.hasErrors()) {
-	    	System.out.println("-- Am intrat in if --");
-	        return modelAndView;
-	    }
+
+		if (result.hasErrors()) {
+			System.out.println("-- Am intrat in if --");
+			System.out.println("------------------------");
+			List<ObjectError> e = result.getAllErrors();
+			for (ObjectError ee : e) {
+				System.out.println(ee);
+			}
+			
+			List<ObjectError> eee = errors.getAllErrors();
+			for (ObjectError ee : eee) {
+				System.out.println(ee);
+			}
+			
+			return modelAndView;
+		}
 
 		System.out.println("MODEL: " + registerDTO);
 
