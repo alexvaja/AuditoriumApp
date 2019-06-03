c6e2836781d30cd4f14f06007c5e90cd7147b1c9
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-03 21:10:34 +0300

    Finished Implementation of Password validator.

--- src/main/java/smartCardUniversity/LOGIN/customValidator/PasswordMatchesValidator.java
+++ src/main/java/smartCardUniversity/LOGIN/customValidator/PasswordMatchesValidator.java
@@ -4,13 +4,20 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import smartCardUniversity.LOGIN.customValidator.annotation.PasswordMatches;
+import smartCardUniversity.LOGIN.models.DTO.RegisterDTO;
 
 public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object>{
 
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {       
+    }
+    
 	@Override
 	public boolean isValid(Object value, ConstraintValidatorContext context) {
-		// TODO Auto-generated method stub
-		return false;
+		System.out.println("AM INTRAT IN VALIUDARE PAROLE");
+		final RegisterDTO registerDTO = (RegisterDTO) value;
+		System.out.println("VALIDARE PAROLE: " + (registerDTO.getPassword().equals(registerDTO.getMatchingPassword())));
+		return registerDTO.getPassword().equals(registerDTO.getMatchingPassword());
 	}
 
 }
