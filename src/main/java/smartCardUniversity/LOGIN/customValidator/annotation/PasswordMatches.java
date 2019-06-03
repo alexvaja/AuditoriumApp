f0e23e3051828794b5a13360a41488acbcb6f8da
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-03 18:59:21 +0300

    Added annotation for matches password.

--- src/main/java/smartCardUniversity/LOGIN/customValidator/annotation/PasswordMatches.java
+++ src/main/java/smartCardUniversity/LOGIN/customValidator/annotation/PasswordMatches.java
@@ -1,5 +1,19 @@
 package smartCardUniversity.LOGIN.customValidator.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+
+import smartCardUniversity.LOGIN.customValidator.PasswordMatchesValidator;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordMatchesValidator.class)
+@Documented
 public @interface PasswordMatches {
 
 }
