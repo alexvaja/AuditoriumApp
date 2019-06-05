f5e2e74651f87ab3866523743df5535fa3073ee0
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-05 16:12:38 +0300

    Added repo class for register.

---
+++ src/main/java/smartCardUniversity/SHARED/repositories/RegisterRepository.java
@@ -0,0 +1,9 @@
+package smartCardUniversity.SHARED.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import smartCardUniversity.SHARED.model.entity.Register;
+
+public interface RegisterRepository extends CrudRepository<Register, Integer> {
+
+}
