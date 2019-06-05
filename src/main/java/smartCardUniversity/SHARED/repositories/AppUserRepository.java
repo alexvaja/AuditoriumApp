47397c1c72754eafac9d5d0ca98370f3bb36f350
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-05 18:39:04 +0300

    Refactored code.

--- src/main/java/smartCardUniversity/SHARED/repositories/AppUserRepository.java
+++ src/main/java/smartCardUniversity/SHARED/repositories/AppUserRepository.java
@@ -1,9 +1,9 @@
 package smartCardUniversity.SHARED.repositories;
+
 import org.springframework.data.repository.CrudRepository;
 
 import smartCardUniversity.SHARED.model.entity.AppUser;
 
-public interface AppUserRepository extends CrudRepository <AppUser, Integer> 
-{
+public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 
 }
