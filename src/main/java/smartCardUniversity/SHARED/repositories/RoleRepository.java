8aa49067b595370052f5dc8f3fb5f2948b0e112e
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-05 18:40:52 +0300

    Added repo for role.

--- src/main/java/smartCardUniversity/SHARED/repositories/RoleRepository.java
+++ src/main/java/smartCardUniversity/SHARED/repositories/RoleRepository.java
@@ -1,5 +1,9 @@
 package smartCardUniversity.SHARED.repositories;
 
-public interface RoleRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import smartCardUniversity.SHARED.model.entity.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Integer>{
 
 }
