11b01f52f8ce9098538f1cc3b5d65a99cce49e63
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-07 16:00:44 +0300

    Added enabled field and refactored all method.

--- src/main/java/smartCardUniversity/SHARED/model/entity/AppUser.java
+++ src/main/java/smartCardUniversity/SHARED/model/entity/AppUser.java
@@ -26,136 +26,140 @@ public class AppUser {
 
 	@Column(name = "inst_email", length = 60, nullable = false, unique = true)
 	private String email;
-	
+
 	@Column(name = "password", length = 120, nullable = false)
 	private String password;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "person_id", nullable = true)
 	private Person person;
-	
-	@ManyToOne/*(cascade = CascadeType.ALL)*/
+
+	@ManyToOne /* (cascade = CascadeType.ALL) */
 	@JoinColumn(name = "college_id", nullable = false)
 	private College userCollege;
+
+	@Column(name = "enable", nullable = false)
+	private boolean enabled;
 	//
-	@OneToOne(/*fetch = FetchType.EAGER*/)
-	@JoinTable(
-			name = "user_role",
-			joinColumns = { @JoinColumn(name = "user_id") },
-			inverseJoinColumns = { @JoinColumn(name = "role_id") }
-			)
+	@OneToOne(/* fetch = FetchType.EAGER */)
+	@JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "role_id") })
 	private Role userRole;
-	
-	@OneToOne(/*fetch = FetchType.EAGER*/)
-	@JoinTable(
-			name = "user_card",
-			joinColumns = { @JoinColumn(name = "user_id") },
-			inverseJoinColumns = { @JoinColumn(name = "card_id") }
-			)
+
+	@OneToOne(/* fetch = FetchType.EAGER */)
+	@JoinTable(name = "user_card", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "card_id") })
 	private Card card;
-	
+
 	@OneToMany(mappedBy = "userBook")
 	private List<BookBooking> bookBookings;
-	
+
 	@OneToMany(mappedBy = "userSeat")
-	private List<SeatBooking> seatBookings; 
-	
+	private List<SeatBooking> seatBookings;
+
 	@OneToMany(mappedBy = "userRoom")
 	private List<RoomBooking> roomBookings;
-	
+
 	@OneToMany(mappedBy = "userOrder")
 	private List<Order> orders;
 
-	public AppUser()
-	{
-		
-	}
-	
-	@Override
-	public String toString() {
-		return "AppUser [id=" + id + ", email=" + email + ", password=" + password;
-	}
-	
-	public AppUser(Integer id, String email, String password, Person person, College userCollege, Role userRole,
-			Card card, List<BookBooking> bookBookings, List<SeatBooking> seatBookings, List<RoomBooking> roomBookings,
-			List<Order> orders) {
+	public AppUser() {
 		super();
-		this.id = id;
-		this.email = email;
-		this.password = password;
-		this.person = person;
-		this.userCollege = userCollege;
-		this.userRole = userRole;
-		this.card = card;
-		this.bookBookings = bookBookings;
-		this.seatBookings = seatBookings;
-		this.roomBookings = roomBookings;
-		this.orders = orders;
+		this.enabled = false;
 	}
 
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Person getPerson() {
 		return person;
 	}
+
 	public void setPerson(Person person) {
 		this.person = person;
 	}
+
 	public College getUserCollege() {
 		return userCollege;
 	}
+
 	public void setUserCollege(College userCollege) {
 		this.userCollege = userCollege;
 	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
 	public Role getUserRole() {
 		return userRole;
 	}
+
 	public void setUserRole(Role userRole) {
 		this.userRole = userRole;
 	}
+
 	public Card getCard() {
 		return card;
 	}
+
 	public void setCard(Card card) {
 		this.card = card;
 	}
+
 	public List<BookBooking> getBookBookings() {
 		return bookBookings;
 	}
+
 	public void setBookBookings(List<BookBooking> bookBookings) {
 		this.bookBookings = bookBookings;
 	}
+
 	public List<SeatBooking> getSeatBookings() {
 		return seatBookings;
 	}
+
 	public void setSeatBookings(List<SeatBooking> seatBookings) {
 		this.seatBookings = seatBookings;
 	}
+
 	public List<RoomBooking> getRoomBookings() {
 		return roomBookings;
 	}
+
 	public void setRoomBookings(List<RoomBooking> roomBookings) {
 		this.roomBookings = roomBookings;
 	}
+
 	public List<Order> getOrders() {
 		return orders;
 	}
+
 	public void setOrders(List<Order> orders) {
 		this.orders = orders;
 	}
@@ -184,5 +188,10 @@ public class AppUser {
 			return false;
 		return true;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "AppUser [" + id + ", " + email + ", " + password + ", " + person + ", " + userCollege + ", " + enabled
+				+ "]";
+	}
 }
