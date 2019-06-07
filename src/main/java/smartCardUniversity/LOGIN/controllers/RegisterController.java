fd2a1fbc111ec9ad409b6bcc75b618ae46aa7505
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-07 15:59:52 +0300

    Implemented event part and added controller for registration conrfim.

--- src/main/java/smartCardUniversity/LOGIN/controllers/RegisterController.java
+++ src/main/java/smartCardUniversity/LOGIN/controllers/RegisterController.java
@@ -1,8 +1,13 @@
 package smartCardUniversity.LOGIN.controllers;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,15 +15,17 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import smartCardUniversity.LOGIN.event.OnRegistrationCompleteEvent;
 import smartCardUniversity.LOGIN.exception.UserAlreadyExistException;
 import smartCardUniversity.LOGIN.models.DTO.RegisterDTO;
 import smartCardUniversity.LOGIN.service.RegisterService;
 import smartCardUniversity.SHARED.model.entity.AppUser;
-import smartCardUniversity.SHARED.model.entity.Register;
+import smartCardUniversity.SHARED.model.entity.VerificationToken;
 
 @Controller
 public class RegisterController implements WebMvcConfigurer {
@@ -27,6 +34,12 @@ public class RegisterController implements WebMvcConfigurer {
 	
 	@Autowired
 	private RegisterService registerService;
+	
+	@Autowired
+	private ApplicationEventPublisher eventPublisher;
+	
+    @Autowired
+    private MessageSource messages;
 
 	@GetMapping("/RegisterView")
 	public ModelAndView getMapping(Model model) {
@@ -52,17 +65,51 @@ public class RegisterController implements WebMvcConfigurer {
 		AppUser registered = createUserAccount(registerDTO, result);			
 
 		if (registered == null) {
-			//result.rejectValue("email", "message.regError");
 			result.rejectValue("email", "message.regError");
 		}
 		
-		if (result.hasErrors()) {
-			System.out.println("SUNT ERORI LA EMAIL");
-			return modelAndView;
-		} else {
-			System.out.println("E OK S-A SALVAT");
+		try {
+			String appUrl = request.getContextPath();
+			eventPublisher.publishEvent(new OnRegistrationCompleteEvent(appUrl, request.getLocale(), registered));
+		} catch (Exception e) {
+			System.out.println("AM PRINS CEVA EXCEPTIE");
 			return modelAndView;
 		}
+		
+		System.out.println("TOTUL A MERS BINE------->>");
+		return modelAndView;
+//		if (result.hasErrors()) {
+//			System.out.println("SUNT ERORI LA EMAIL");
+//			return modelAndView;
+//		} else {
+//			System.out.println("E OK S-A SALVAT");
+//			return modelAndView;
+//		}
+	}
+	
+	@GetMapping("/registrationConfirm")
+	public String confirmationRegistration(WebRequest request, Model model, @RequestParam("token") String token) {
+		Locale locale = request.getLocale();
+	     
+	    VerificationToken verificationToken = registerService.getVerificationToken(token);
+	    if (verificationToken == null) {
+	        String message = messages.getMessage("auth.message.invalidToken", null, locale);
+	        model.addAttribute("message", message);
+	        return "redirect:/badUser.html?lang=" + locale.getLanguage();
+	    }
+	     
+	    AppUser user = verificationToken.getAppUser();
+	    Calendar cal = Calendar.getInstance();
+	    if ((verificationToken.getExpiryDate().getTime() - cal.getTime().getTime()) <= 0) {
+	        String messageValue = messages.getMessage("auth.message.expired", null, locale);
+	        model.addAttribute("message", messageValue);
+	        return "redirect:/badUser.html?lang=" + locale.getLanguage();
+	    } 
+	     
+	    user.setEnabled(true); 
+	    registerService.saveRegisteredUser(user); 
+	    return "redirect:/views/LoginView.html?lang=" + request.getLocale().getLanguage();
+		
 	}
 	
 	private AppUser createUserAccount(RegisterDTO registerDTO, BindingResult result) {
@@ -78,18 +125,3 @@ public class RegisterController implements WebMvcConfigurer {
 		return registered;
 	}
 }
-
-
-
-
-//System.out.println("-- Am intrat in if --");
-//System.out.println("------------------------");
-//List<ObjectError> e = result.getAllErrors();
-//for (ObjectError ee : e) {
-//	System.out.println(ee);
-//}
-//
-//List<ObjectError> eee = errors.getAllErrors();
-//for (ObjectError ee : eee) {
-//	System.out.println(ee);
-//}
