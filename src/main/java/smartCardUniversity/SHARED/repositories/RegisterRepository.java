ac13dd1e9225e267490b2cfd53905f03e8d6a63b
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-10 14:01:48 +0300

    Deleted entites.

--- src/main/java/smartCardUniversity/SHARED/repositories/RegisterRepository.java
+++
@@ -1,9 +0,0 @@
-package smartCardUniversity.SHARED.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import smartCardUniversity.SHARED.model.entity.Register;
-
-public interface RegisterRepository extends CrudRepository<Register, Integer> {
-
-}
