5666834209bbec13303d0e83e832c7caf79abbe9
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-10 10:36:48 +0300

    Added anntotation for repository class.

--- src/main/java/smartCardUniversity/LOGIN/event/listener/RegistrationListener.java
+++ src/main/java/smartCardUniversity/LOGIN/event/listener/RegistrationListener.java
@@ -2,6 +2,8 @@ package smartCardUniversity.LOGIN.event.listener;
 
 import java.util.UUID;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.MessageSource;
@@ -10,13 +12,18 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import smartCardUniversity.LOGIN.event.OnRegistrationCompleteEvent;
-import smartCardUniversity.LOGIN.service.IRegisterService;
+import smartCardUniversity.LOGIN.service.RegisterService;
 import smartCardUniversity.SHARED.model.entity.AppUser;
+import smartCardUniversity.SHARED.model.entity.VerificationToken;
+import smartCardUniversity.SHARED.repositories.VerificationTokenRepository;
 
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
 
 	@Autowired
-	private IRegisterService service;
+	private RegisterService service;
+	
+	@Autowired
+	private VerificationTokenRepository tokenRepository;
 	
 	@Autowired
 	private MessageSource messages;
@@ -29,29 +36,49 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
 	
 	@Override
 	public void onApplicationEvent(OnRegistrationCompleteEvent event) {
+		System.out.println("Am intrat in constructorul de la LISTENER");
 		this.confirmRegistration(event);
 	}
 	
+	@Transactional
 	private void confirmRegistration(OnRegistrationCompleteEvent event) {
-		
+		System.out.println("Am intrat in functia apelata de constrictor");
 		AppUser user = event.getAppUser();
+		System.out.println("USER: " + user);
 		String token = UUID.randomUUID().toString();
-		service.createVerificationTokenForUser(user, token);
+		System.out.println("TOKEN: " + token);
+		
+		System.out.println("Salvare token");
+		VerificationToken newToken = new VerificationToken(token, user);
+		System.out.println("TOKENUL FORMAT: " + newToken);
+		//tokenRepository.save(newToken);
+		//service.createVerificationTokenForUser(user, token);
+		System.out.println("TOKEN-UL a fost salvat");
 		
 		SimpleMailMessage email = constructEmailMessage(event, user, token);
 		mailSender.send(email);
 	}
 	
     private final SimpleMailMessage constructEmailMessage(final OnRegistrationCompleteEvent event, final AppUser user, final String token) {
-        final String recipientAddress = user.getEmail();
-        final String subject = "Registration Confirmation";
-        final String confirmationUrl = event.getAppUrl() + "/registrationConfirm.html?token=" + token;
-        final String message = messages.getMessage("message.regSucc", null, event.getLocale());
-        final SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo(recipientAddress);
-        email.setSubject(subject);
-        email.setText(message + " \r\n" + confirmationUrl);
-        email.setFrom(env.getProperty("support.email"));
-        return email;
+    	System.out.println("Am intrat in constructie mail;");
+    	
+    	final String recipientAddress = user.getEmail();
+        System.out.println("1");
+    	final String subject = "Registration Confirmation";
+        System.out.println("2");
+    	final String confirmationUrl = event.getAppUrl() + "/registrationConfirm.html?token=" + token;
+        
+    	final String message = "Ana are mere!  ";//messages.getMessage("message.regSucc", null, event.getLocale());
+        
+    	final SimpleMailMessage email = new SimpleMailMessage();
+        
+    	email.setTo(recipientAddress);
+        
+    	email.setSubject(subject);
+        
+    	email.setText(message + " \r\n" + confirmationUrl);
+        
+    	email.setFrom(env.getProperty("support.email"));
+    	return email;
     }
 }
