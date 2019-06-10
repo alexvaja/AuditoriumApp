55a7fd9e9a9d9075b38e247450b933de13d26c1f
WW930\A707350 <alexandru_vaja@yahoo.com>
2019-06-10 10:42:39 +0300

    Added annotation for repository class.

--- src/main/java/smartCardUniversity/SHARED/repositories/VerificationTokenRepository.java
+++ src/main/java/smartCardUniversity/SHARED/repositories/VerificationTokenRepository.java
@@ -1,9 +1,11 @@
 package smartCardUniversity.SHARED.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import smartCardUniversity.SHARED.model.entity.VerificationToken;
 
+@Repository
 public interface VerificationTokenRepository extends CrudRepository<VerificationToken, Integer>{
 
 	VerificationToken findByToken(String token);
